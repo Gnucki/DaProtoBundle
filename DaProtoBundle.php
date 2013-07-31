5f3f39db1acae7e6a107e6d429c35452b6de41aa
Gnucki <tprelot@gmail.com>
2013-07-31 09:58:24 +0200

    Allow to change the alias of the bundle

--- DaProtoBundle.php
+++ DaProtoBundle.php
@@ -3,7 +3,12 @@
 namespace Da\ProtoBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Da\ProtoBundle\DependencyInjection\DaProtoExtension;
 
 class DaProtoBundle extends Bundle
 {
+    public function __construct()
+    {
+        $this->extension = new DaProtoExtension();
+    }
 }
